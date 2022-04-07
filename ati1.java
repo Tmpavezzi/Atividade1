f47c00a2aaee37ee11908ab18b5106f5412bcc05
Tmpavezzi <102332493+Tmpavezzi@users.noreply.github.com>
2022-04-07 19:36:43 -0300

    Create ati1.java

---
+++ ati1.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+
+
+
+class Main {
+  public static void main(String[] args) {
+     Scanner entrada = new Scanner (System.in);
+       System.out.print("um numero ?");
+       int N = entrada.nextInt();
+    
+
+    if(N <= -1){
+      System.out.print("Negativo");
+    } else if ( N >= 1){
+      System.out.print("Positivo ");
+      
+    }
+    
+        
+        
+    }
+    
+
+  }
